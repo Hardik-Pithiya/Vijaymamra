9924969dec674f12d258d445e5203e16d5476d4b
darshitvadadoriya <darshit@sanskartechnolab.com>
2024-09-30 15:26:19 +0530

    update gatepass out in and outwards and validations

--- vijaymamra/vijaymamra/doctype/gate_pass_out/gate_pass_out.js
+++ vijaymamra/vijaymamra/doctype/gate_pass_out/gate_pass_out.js
@@ -12,7 +12,105 @@ frappe.ui.form.on('Gate Pass Out', {
 				}
 			};
 		});
-	}
+
+		frm.set_query('purchase_receipt', 'invoice_details', function (doc, cdt, cdn) {
+			let d = locals[cdt][cdn];
+			return {
+				filters: {
+					'custom_gate_pass_out': 0,
+					'docstatus':1
+				}
+			};
+		});
+
+	},
+	refresh: function (frm) {
+        // Call check_wards on form refresh
+       frm.doc.invoice_details.forEach(function(row, index) {
+            check_wards(frm, 'Invoice Details', row.name);
+        });
+    }
 
 });
+
+
+frappe.ui.form.on('Invoice Details', {
+	refresh:function(frm,cdt,cdn){
+		check_wards(frm,cdt,cdn)
+	},
+	wards:function(frm,cdt,cdn){
+		check_wards(frm,cdt,cdn)
+		
+	}
+})
+
+
+
+
+
+
+function check_wards(frm, cdt, cdn) {
+    var data = locals[cdt][cdn]; // Get the current row
+
+    // Check if the child table has rows
+    if (frm.doc.invoice_details && frm.doc.invoice_details.length > 0) {
+        // Loop through all rows in the grid
+        frm.doc.invoice_details.forEach(function (row, index) {
+            // Check the value of 'wards' in each row and set properties accordingly
+            if (row.wards === "In Wards") {
+                
+				// // Show fields
+				frm.set_df_property('invoice_details', 'read_only', 1, frm.doc.name, 'invoice_number', row.name)
+				frm.set_df_property('invoice_details', 'read_only', 0, frm.doc.name, 'purchase_receipt', row.name)
+
+				frm.set_df_property('invoice_details', 'read_only', 0, frm.doc.name, 'supplier', row.name)
+				frm.set_df_property('invoice_details', 'read_only', 0, frm.doc.name, 'supplier_name', row.name)
+				frm.set_df_property('invoice_details', 'read_only', 0, frm.doc.name, 'pr_weight', row.name)
+				frm.set_df_property('invoice_details', 'read_only', 0, frm.doc.name, 'pr_qty', row.name)
+				frm.set_df_property('invoice_details', 'read_only', 0, frm.doc.name, 'pr_bag', row.name)
+				
+                // Set read-only and hidden properties for the respective fields in the child table
+                frm.fields_dict['invoice_details'].grid.grid_rows[index].toggle_editable('invoice_number', false);
+                frm.fields_dict['invoice_details'].grid.grid_rows[index].toggle_editable('purchase_receipt', true);
+
+                // Clear fields
+                row.invoice_number = "";
+                row.customer_code = "";
+                row.customer_name = "";
+                row.total_quantity = "";
+                row.total_bag = "";
+            }
+			if (row.wards === "Out Wards") {
+
+				
+
+				frm.set_df_property('invoice_details', 'read_only', 0, frm.doc.name, 'invoice_number', row.name)
+				frm.set_df_property('invoice_details', 'read_only', 1, frm.doc.name, 'purchase_receipt', row.name)
+				frm.set_df_property('invoice_details', 'read_only', 1, frm.doc.name, 'supplier', row.name)
+				frm.set_df_property('invoice_details', 'read_only', 1, frm.doc.name, 'supplier_name', row.name)
+				frm.set_df_property('invoice_details', 'read_only', 1, frm.doc.name, 'pr_weight', row.name)
+				frm.set_df_property('invoice_details', 'read_only', 1, frm.doc.name, 'pr_qty', row.name)
+				frm.set_df_property('invoice_details', 'read_only', 1, frm.doc.name, 'pr_bag', row.name)
+
+				
+                // Set blank value
+                frm.fields_dict['invoice_details'].grid.grid_rows[index].toggle_editable('invoice_number', true);
+                frm.fields_dict['invoice_details'].grid.grid_rows[index].toggle_editable('purchase_receipt', false);
+
+              
+                // Clear fields
+                row.purchase_receipt = "";
+                row.supplier = "";
+                row.supplier_name = "";
+                row.pr_qty = "";
+                row.pr_bag = "";
+            }
+        });
+		// Refresh the child table to apply changes
+		frm.refresh_field('invoice_details');
+    }
+
+    
+}
+
 
--- vijaymamra/vijaymamra/doctype/gate_pass_out/gate_pass_out.py
+++ vijaymamra/vijaymamra/doctype/gate_pass_out/gate_pass_out.py
@@ -6,8 +6,19 @@ import json
 from frappe.model.document import Document
 
 class GatePassOut(Document):
-	def on_submit(self):
-		si_data = self.invoice_details
-
-		for item in si_data:
-			frappe.db.set_value("Sales Invoice", item.get('invoice_number'), "custom_gate_pass_out", 1)
+    def on_submit(self):
+        si_data = self.invoice_details
+        
+        for item in si_data:
+            if item.invoice_number:
+                frappe.db.set_value("Sales Invoice", item.invoice_number, "custom_gate_pass_out", 1)
+            if item.purchase_receipt:
+                frappe.db.set_value("Purchase Receipt", item.purchase_receipt, "custom_gate_pass_out", 1)
+    def before_cancel(self):
+        si_data = self.invoice_details
+        for item in si_data:
+            if item.invoice_number:
+                frappe.db.set_value("Sales Invoice", item.invoice_number, "custom_gate_pass_out", 0)
+            if item.purchase_receipt:
+                frappe.db.set_value("Purchase Receipt", item.purchase_receipt, "custom_gate_pass_out", 0)
+        
